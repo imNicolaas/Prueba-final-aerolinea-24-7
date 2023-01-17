379cbc1cf88a8d6d97483fe11a206587ccccd18c
imNicolaas <119824434+imNicolaas@users.noreply.github.com>
2023-01-17 14:39:26 -0500

    Update Registro_usuario.php

--- PHP/Registro_usuario.php
+++ PHP/Registro_usuario.php
@@ -9,7 +9,6 @@ $contrasena = $_POST['contrasena'];
 $query = "INSERT INTO usuarios(nombre_completo, correo, contrasena) 
                   values('$nombre_completo', '$correo', '$contrasena')";
 
-//estaba bien lo del video, no me estaba funcionnado por que en mi INSERT INTO tenia como segundo campo correo, y el en el phpmyadmin en la base de datos el segundo campo se llamaba usuario, ademas en mi INSERT INTO como tercer campo tenia contrasena y en la base de datos de phpmyadmin el tercer campo se llamaba contraseña con ñ
 
 $verificar_correo = mysqli_query($conexion, "select * from usuarios where correo = '$correo'");
 
